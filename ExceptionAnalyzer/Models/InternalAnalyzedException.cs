020c7660d42a7f3ede6507dbbfc950334b0f2a40
Lukas Schachner <lukas@schachner.dev>
2023-04-01 10:28:51 +0200

    chore: add rider files to gitignore and fix spelling

---
+++ ExceptionAnalyzer/Models/InternalAnalyzedException.cs
@@ -0,0 +1,20 @@
+﻿using System.Runtime.CompilerServices;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+[assembly:InternalsVisibleTo("ExceptionAnalyzer.Tests")]
+
+namespace ExceptionAnalyzer.Models
+{
+    internal class InternalAnalyzedException
+    {
+        [JsonProperty("errorAnalysis")]
+        internal string ErrorAnalysis { get; set; }
+        [JsonProperty("userMessage")]
+        internal string UserMessage { get; set; }
+        [JsonProperty("developerDetails")]
+        internal string DeveloperDetails { get; set; }
+        [JsonProperty("solutions")]
+        internal List<string> Solutions { get; set; }
+    }
+}
